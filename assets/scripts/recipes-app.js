7549b9bb82dd21bd1d2622e04e03438ca74e930c
Nora Flynn <nora.flynn@aol.com>
2026-09-02 17:04:43 +0100

    'Add recipe' modal initial structure

--- assets/scripts/recipes-app.js
+++ assets/scripts/recipes-app.js
@@ -1,46 +1,56 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addTask() {
-    const task = inputBox.value.trim();
-    if (!task) {
-        alert("Please write down a task");
-        return;
-    }
+function addRecipePopup(input) {
+  const task = inputBox.value.trim();
+  if (!task) {
+    alert("Please write a recipe name");
+    return;
+  }
 
-    const li = document.createElement("li");
-    
-    li.innerHTML = `
+  // show popup and bind input value to title
+  $("#recipeAddModal").modal("show");
+  var text = inputBox.value;
+  const h1Modal = document.querySelector("#recipeAddModalTitle");
+  var sanitizeHTML = function (str) {
+    var temp = document.createElement("div");
+    temp.textContent = str;
+    return temp.innerHTML;
+  }; // (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+  h1Modal.innerText = sanitizeHTML(text);
+
+  inputBox.value = "";
+}
+
+function addRecipe() {
+  const li = document.createElement("li");
+
+  li.innerHTML = `
         <label>
             <input type="checkbox">
             <span>${task}</span>
         </label>
         <span class="edit-btn">Edit</span>
         `;
-    
-    listContainer.appendChild(li);
-    inputBox.value = "";
 
-    const checkbox = li.querySelector("input");
-    const editBtn = li.querySelector(".edit-btn");
-    const taskSpan = li.querySelector("span");
+  listContainer.appendChild(li);
+  inputBox.value = "";
 
-    checkbox.addEventListener("click", function () {
-        li.classList.toggle("completed", checkbox.checked);
-        console.log("completed toggle");
-    });
+  const checkbox = li.querySelector("input");
+  const editBtn = li.querySelector(".edit-btn");
+  const taskSpan = li.querySelector("span");
 
-    editBtn.addEventListener("click", function () {
-        const update = prompt("Edit task:", taskSpan.textContent);
-        if (update !== null) {
-            taskSpan.textContent = update;
-            li.classList.remove("completed");
-            checkbox.checked = false;
-        }
-    });
+  checkbox.addEventListener("click", function () {
+    li.classList.toggle("completed", checkbox.checked);
+    console.log("completed toggle");
+  });
+
+  editBtn.addEventListener("click", function () {
+    const update = prompt("Edit task:", taskSpan.textContent);
+    if (update !== null) {
+      taskSpan.textContent = update;
+      li.classList.remove("completed");
+      checkbox.checked = false;
+    }
+  });
 }
-
-
-
-
-
